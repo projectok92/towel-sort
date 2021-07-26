49b4c9558be9e027c5d269d66345fa564cfabf55
projectok92 <lexkoval92@gmail.com>
2021-07-26 18:52:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  let toSort = [];
+  matrix.map((el, idx) => idx % 2 === 0 ? toSort.push(el.sort((a, b) => a - b)) : toSort.push(el.sort((a, b) => a - b).reverse()));
+  let sorted = toSort.reduce((fin, el) => [...fin, ...el], []);
+  return sorted;
 }
